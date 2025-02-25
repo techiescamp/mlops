216fe819fa90a8b55604789ede58df2050ac8d03
SravyaCrunchOps <sravya@crunchops.com>
2025-02-25 10:45:53 +0530

    [MODIFY] emp-attr model_class.py

--- employee-attrition/employee_attrition_ui/app.py
+++ employee-attrition/employee_attrition_ui/app.py
@@ -1,51 +1,54 @@
-import os
-import pickle
-import sys
-import numpy as np
 import pandas as pd
+import requests
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
+import os
+
 
 # sart flask app
 app = Flask(__name__)
-
-# Cors
 CORS(app)
 
-# get the absolute path of the current script
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-# move one level up to the project root directory
-PROJECT_DIR = os.path.dirname(BASE_DIR)
-
-# Define the path to the model file inside the "model" directory
-MODEL_DIR = os.path.join(PROJECT_DIR, "employee_attrition_kserve_model")
-sys.path.append(MODEL_DIR)
-
-# import model class here
-from model_class import EmployeeAttritionModel
-model_path = os.path.join(MODEL_DIR, "my_model_lr.pkl")
-
-# load the model and the scaler
-if os.path.isfile(model_path):
-    with open(model_path, "rb") as f:
-        model = pickle.load(f)
-else:
-    raise FileNotFoundError(f"Model file not found at {model_path}")
-
+KSERVE_ENDPOINT = "http://52.151.18.124:30080/v1/models/employee-attrition:predict"
 
 # home page
-@app.route('/')
+@app.route("/")
 def index():
     return render_template('index.html')
 
-# start route
-@app.route('/predict', methods=['POST'])
+# predict 
+@app.route("/predict", methods=["POST"])
 def predict():
     data = request.get_json(force=True)
-    predicition = model.predict([data])
-    result = "Left" if predicition[0] else "Stayed"
-    return jsonify({"prediction": result})
+
+    # convert input to kserve expected format
+    payload = {"instances": [data]} 
+    print('payload: ', payload)
+
+    # send request to kserve endpoint
+    try:
+        response = requests.post(KSERVE_ENDPOINT, json=payload)
+        print('res: ', response)
+        if response.status_code == 200:
+            response_data = response.json()
+            print(response_data)
+
+            # extract predicition result
+            prediction = response_data.get("predictions", [[]])[0]
+            print(prediction)
+            result = "Left" if prediction else "Stayed"
+            print('result: ', result)
+            return jsonify({"prediction": result})
+        else:
+            error_msg = f"KServe returned status code {response.status_code}: {response.text}"
+            print(error_msg)
+            return jsonify({"error": error_msg}), 500
+
+    except Exception as e:
+        error_msg = f"Exception occurred: {str(e)}"
+        print(error_msg)
+        return jsonify({"error": error_msg}), 500
+
 
 if __name__ == "__main__":
     app.run(debug=True)
