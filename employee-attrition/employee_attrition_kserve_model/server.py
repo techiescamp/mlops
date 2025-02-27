3a43e1bf6e4cb1e9b7c03379eabaa59abf825407
SravyaCrunchOps <sravya@crunchops.com>
2025-02-27 21:42:39 +0530

    [MODIFY] emp-attr: server.py

--- employee-attrition/employee_attrition_kserve_model/server.py
+++ employee-attrition/employee_attrition_kserve_model/server.py
@@ -1,3 +1,4 @@
+# emp_attr_model/server.py
 import pickle
 import os
 from kserve import Model, ModelServer
@@ -7,11 +8,11 @@ class EmployeeAttritionServer(Model):
     def __init__(self, name):
         super().__init__(name)
         self.name = name
-        self.model = model
+        self.model = None
         self.ready = False
 
     def load(self):
-        model_dir = os.getenv("MODEL_DIR", "/mnt/models") # kserve mounts storageUri here
+        model_dir = os.getenv("MODEL_DIR", "/mnt/models")  # KServe mounts storageUri here
         with open(os.path.join(model_dir, "model.pkl"), "rb") as f:
             model = pickle.load(f)
         with open(os.path.join(model_dir, "scaler.pkl"), "rb") as f:
@@ -22,13 +23,14 @@ class EmployeeAttritionServer(Model):
             column_names = pickle.load(f)
         with open(os.path.join(model_dir, "categories.pkl"), "rb") as f:
             categories = pickle.load(f)
+
         self.model = EmployeeAttritionModel(model, scaler, encoder, column_names, categories)
         self.ready = True
 
     def predict(self, payload, headers=None):
-        return self.model.predict(payload) # call custom predict
+        return self.model.predict(payload)  # Call your custom predict
 
-if __name__=="__main__":
+if __name__ == "__main__":
     model = EmployeeAttritionServer("employee-attrition")
     model.load()
     ModelServer().start([model])
